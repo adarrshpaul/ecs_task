bdfd6d9db7625cda2fb90c07cd0c4f369b16c7ad
adarrsh_dev <adarrshdeveloper@gmail.com>
2023-11-30 09:47:29 +0530

    cdk minor syntax fixes

--- iac-cdk/lib/iac-cdk-stack.js
+++ iac-cdk/lib/iac-cdk-stack.js
@@ -1,9 +1,8 @@
-const { Stack, Construct } = require('aws-cdk-lib');
+const { Stack, Construct, Duration } = require('aws-cdk-lib');
 const { Vpc } = require('aws-cdk-lib/aws-ec2');
-const { Cluster, FargateTaskDefinition, FargateService } = require('aws-cdk-lib/aws-ecs');
-const { Role, PolicyDocument, CompositePrincipal, PolicyStatement, ManagedPolicy } = require('aws-cdk-lib/aws-iam');
-const { LogGroup } = require('aws-cdk-lib/aws-logs');
-const { Duration } = require('aws-cdk-lib/aws-steps');
+const { Cluster, FargateTaskDefinition, FargateService, ContainerImage } = require('aws-cdk-lib/aws-ecs');
+const { Role, PolicyDocument, CompositePrincipal, PolicyStatement, ManagedPolicy, Effect, ServicePrincipal } = require('aws-cdk-lib/aws-iam');
+const { LogGroup, RetentionDays } = require('aws-cdk-lib/aws-logs');
 const { CfnOutput } = require('aws-cdk-lib/aws-cloudformation');
 
 class TestStack extends Stack {
@@ -14,15 +13,11 @@ class TestStack extends Stack {
     const customPolicyDoc = new PolicyDocument({
       statements: [
         new PolicyStatement({
-          effect: 'ALLOW',
+          effect: Effect.ALLOW,
           resources: ['*'],
-          actions: ['ecr:*', 'logs:*', 'cloudwatch:PutMetricData', 'autoscaling:DescribeAutoScalingGroups', 'autoscaling:TerminateInstanceInAutoScalingGroup'],
+          actions: ['ecr:*', 'cloudwatch:PutMetricData', 'autoscaling:DescribeAutoScalingGroups', 'autoscaling:TerminateInstanceInAutoScalingGroup'],
         }),
       ],
-    });
-
-    const trust = new CompositePrincipal({
-      service: 'ecs-tasks.amazonaws.com',
     });
 
     // Create a VPC for our resources to live in:
@@ -33,20 +28,17 @@ class TestStack extends Stack {
 
     // Create a role with the custom policy and managed policies for our ECS tasks to assume:
     const taskRole = new Role(this, 'taskRoleEcs', {
-      assumedBy: trust,
+      assumedBy: new ServicePrincipal('ecs.amazonaws.com'),
       inlinePolicies: { 'custom_policy': customPolicyDoc },
       managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName('AWSXRayDaemonWriteAccess')],
     });
 
     // Add any managed policies you want to apply to this role (e.g., AWSXRayDaemonWriteAccess) here...
     const executionRole = new Role(this, 'executionRoleEcs', {
-      assumedBy: trust,
+      assumedBy: new ServicePrincipal('ecs.amazonaws.com'),
       inlinePolicies: { 'custom_policy': customPolicyDoc },
       managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName('AWSXRayDaemonWriteAccess')],
     });
-
-    // Create a separate execution role for ECS tasks to assume (optional)... Here...
-    // Add any managed policies you want to apply to this role (e.g., AWSXRayDaemonWriteAccess) here...
 
     // Define your application container(s) here...
     const appContainer = {};
@@ -56,43 +48,33 @@ class TestStack extends Stack {
 
     // Create a CloudWatch log group for our ECS tasks' logs and set its retention period to 14 days (optional):
     const logGroup = new LogGroup(this, 'MyLogGroup', {
-      retention: Duration.days(14),
+      retention: RetentionDays.ONE_MONTH
     });
 
     const expressApp = new FargateTaskDefinition(this, 'AppTaskDefinition', {
-      cpu: 512,
+      cpu: 0.5,
       memoryLimitMiB: 512,
       executionRole: executionRole,
       taskRole: taskRole,
     });
 
     expressApp.addContainer('ExpressAppContainer', {
-      image: 'iluvyou3000/xray_instrumented_app:16',
-      cpu: 256,
+      image: ContainerImage.fromRegistry('iluvyou3000/xray_instrumented_app:latest'),
+      cpu: 0.25,
     });
 
-    const xrayDaemon = new FargateTaskDefinition(this, 'XRayDaemonTaskDefinition', {
-      cpu: 256,
-      executionRole: executionRole,
-      taskRole: taskRole,
-    });
-
-    xrayDaemon.addContainer('XRayDaemonContainer', {
-      image: 'amazon/aws-xray-daemon',
-      cpu: 256,
+    expressApp.addContainer('XRayDaemonContainer', {
+      image: ContainerImage.fromRegistry('amazon/aws-xray-daemon'),
+      cpu: 0.25
     });
 
     // Now, you can create a service and associate it with your task definition
     const expressAppService = new FargateService(this, 'ExpressAppService', {
       cluster,
       taskDefinition: expressApp,
-      desiredCount: 2, // Adjust as needed
+      desiredCount: 1, // Adjust as needed
     });
 
-    // Output the service URL
-    new CfnOutput(this, 'ServiceUrl', {
-      value: expressAppService.loadBalancer.loadBalancerDnsName,
-    });
   }
 }
 
